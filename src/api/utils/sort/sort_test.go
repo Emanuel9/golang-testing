83e312f35e5950c636b51be4231fe37915ce0442
emanueldumitru <emanuel.dumitru9@gmail.com>
2020-02-15 16:46:17 +0200

    Init testing project

---
+++ src/api/utils/sort/sort_test.go
@@ -0,0 +1,31 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestBubbleSortOrderDesc(t *testing.T) {
+	// Init
+	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+
+	// Execution
+	BubbleSort(elements)
+
+	//Validation
+	if elements[0] != 9 {
+		t.Error("first element should be 9")
+	}
+
+	if elements[len(elements)-1] != 0 {
+		t.Error("last element should be 0")
+	}
+
+}
+
+func TestBubbleSortAlreadySorted(t *testing.T) {
+	//Init
+	elements := []int{5, 3, 2, 1}
+
+	//Execution
+	BubbleSort(elements)
+}
